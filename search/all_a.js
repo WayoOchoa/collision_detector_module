ba4111b60140b79b663f62c297c756c71ae1330c
WayoOchoa <WayoOchoa@users.noreply.github.com>
2024-03-22 08:45:30 +0000

    deploy: 395d98bf81f66042ebaaf45e1383185bf75b9746

--- search/all_a.js
+++ search/all_a.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['transferdata_0',['transferData',['../classcoldetector_1_1CollisionDetector.html#a41e80df83d6f79de79798674bef21156',1,'coldetector::CollisionDetector']]],
   ['transferdatacallback_1',['TransferDataCallback',['../classCollisionNode.html#ad6e10b8e744b58de9ceea86c2a5a5661',1,'CollisionNode']]],
-  ['triangulatepoints_2',['TriangulatePoints',['../classcoldetector_1_1CollisionDetector.html#a9b07097107a9acbf91bb5e338998d2c6',1,'coldetector::CollisionDetector']]]
+  ['triangulatepoints_2',['TriangulatePoints',['../classcoldetector_1_1CollisionDetector.html#a947c6ea7d547a695985b2fe02df10c00',1,'coldetector::CollisionDetector']]]
 ];
